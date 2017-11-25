1337e7b96c2017fce03674f50ea595a0c91b791c
Alex Lokshin <alokshin@gmail.com>
2017-11-24 23:08:18 -0500

    Adding a unit test for CI/CD

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send({Status: 'OK'}))
-
-app.listen(3000, () => console.log('Started the server on port 3000.'))
